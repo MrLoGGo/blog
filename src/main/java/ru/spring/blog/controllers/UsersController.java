9924b5c9a58d51f8ca13a0d73e4c8ac4cca335d7
MrLoGGo <Logo123444445@gmail.com>
2022-08-28 22:58:41 +0300

    registration is done

--- src/main/java/ru/spring/blog/controllers/UsersController.java
+++ src/main/java/ru/spring/blog/controllers/UsersController.java
@@ -2,6 +2,7 @@ package ru.spring.blog.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +14,20 @@ import ru.spring.blog.models.User;
 @RequestMapping("/users")
 public class UsersController {
 
-    private UserDAO userDAO;
+    private final UserDAO userDAO;
 
     @Autowired
     public UsersController(UserDAO userDAO){
         this.userDAO =userDAO;
     }
+
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute("users", userDAO.index());
+        // Получаем всех людей из DAO и передаем на отображение в представление
+        return "users/index";
+    }
+
 
     @GetMapping("/new")
     public String newUser(@ModelAttribute("user")User user){
